10cae7e73fa7591eca132a21808953d67b5ede16
waseem abbas <waseemofficee@gamil.com>
2025-10-25 20:31:49 +0500

    `Removed unused routes and components, updated home page to use Remotion Player`

--- my-remotion-video/app/routes.ts
+++ my-remotion-video/app/routes.ts
@@ -2,6 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("./home.tsx"),
-  route("/api/lambda/progress", "./progress.tsx"),
-  route("/api/lambda/render", "./render.tsx"),
+
 ] satisfies RouteConfig;
